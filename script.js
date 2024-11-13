c3539bc770cb64ffc60d2b5a0b05f58c2671079e
yNightk <sarinhaboozinha@gmail.com>
2024-11-12 21:07:18 -0300

    Script.js

--- script.js
+++ script.js
@@ -4,24 +4,3 @@ document.getElementById("contactForm").addEventListener("submit", function(event
     // Simulação de envio de mensagem
     document.getElementById("formMessage").textContent = "Obrigado por entrar em contato! Em breve responderemos.";
     document.getElementById("contactForm").reset();
-    
-    const carouselSlide = document.querySelector('.carousel-slide');
-    const images = document.querySelectorAll('.carousel-slide img');
-    const prevButton = document.querySelector('.prev');
-    const nextButton = document.querySelector('.next');
-
-    let counter = 0;
-
-    function showSlide() {
-        carouselSlide.style.transform = `translateX(${-counter * 100}%)`;
-    }
-
-    nextButton.addEventListener('click', () => {
-        counter = (counter + 1) % images.length;
-        showSlide();
-    });
-
-    prevButton.addEventListener('click', () => {
-        counter = (counter - 1 + images.length) % images.length;
-        showSlide();
-    });
